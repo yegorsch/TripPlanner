1d308c836225ac1a43780fac4d42937523b053fa
Yegor Chsherbatykh <yegor.chsherbatykg@nu.edu.kz>
2017-03-23 19:47:26 +0600

    Initial Commit

---
+++ Planner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Planner
+//
+//  Created by Егор on 3/23/17.
+//  Copyright © 2017 Yegor's Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
